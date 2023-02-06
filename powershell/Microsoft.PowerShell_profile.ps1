7c0cc9e7b59ef2235a662d0293ec576c8da57d41
unoun <ounouno+github@gmail.com>
2023-02-06 23:54:31 +0900

    Add Microsoft.PowerShell_profile.ps1

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+$ScriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+
+#
+oh-my-posh init pwsh --config $ScriptDir\config.omp.json | Invoke-Expression
+
+#
+[System.Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+$OutputEncoding = [System.Console]::OutputEncoding
