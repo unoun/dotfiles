1f1416d7de7f157eceea20a48b7eb45c3bfb5f83
unoun <ounouno+github@gmail.com>
2023-02-07 23:45:08 +0900

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+#
 $ScriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
 #
@@ -6,3 +7,9 @@ oh-my-posh init pwsh --config $ScriptDir\config.omp.json | Invoke-Expression
 #
 [System.Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
 $OutputEncoding = [System.Console]::OutputEncoding
+
+#
+$env:JUPYTER_CONFIG_DIR = "$(Split-Path -Path $ScriptDir)\.jupyter"
+
+#
+Remove-Variable ScriptDir
